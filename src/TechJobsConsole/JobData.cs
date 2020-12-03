0996bec2bbb9ab8e1220177d60ad4ee408c4b3bc
Aimee Bhasme <aimee.rae612@gmail.com>
2020-12-03 10:09:56 -0600

    case insensitive complete

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -47,9 +47,9 @@ namespace TechJobsConsole
 
             foreach (Dictionary<string, string> row in AllJobs)
             {
-                string aValue = row[column];
+                string aValue = row[column].ToLower();
 
-                if (aValue.Contains(value))
+                if (aValue.Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
@@ -68,9 +68,9 @@ namespace TechJobsConsole
             {
                 foreach (string result in row.Values)
                 {
-                    string aValue = result;
+                    string aValue = result.ToLower();
 
-                    if (aValue.Contains(value))
+                    if (aValue.Contains(value.ToLower()))
                     {
                         jobs.Add(row);
                         break;
