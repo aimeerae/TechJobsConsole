0d423786e92d9e2e4ab5e2c966aa0507441cf956
Aimee Bhasme <aimee.rae612@gmail.com>
2020-12-03 08:35:04 -0600

    Find by value done

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -57,6 +57,30 @@ namespace TechJobsConsole
 
             return jobs;
         }
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            // load data, if not already loaded
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (string result in row.Values)
+                {
+                    string aValue = result;
+
+                    if (aValue.Contains(value))
+                    {
+                        jobs.Add(row);
+                        break;
+                    }
+                }
+            }
+        
+
+            return jobs;
+        }
 
         /*
          * Load and parse data from job_data.csv
